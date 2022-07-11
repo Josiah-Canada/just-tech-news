d8511136d582dca198eb4f7efff00e260db2deb5
Josiah-Canada <josiahcanada@yahoo.com>
2022-07-10 21:35:04 -0400

    difficulties again

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
@@ -12,6 +9,8 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
