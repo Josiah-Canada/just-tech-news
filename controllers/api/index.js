8a3d3bb2fb87d178620d022efec70a91a27f5c8b
Josiah-Canada <josiahcanada@yahoo.com>
2022-07-10 21:39:33 -0400

    difficulties again

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const homeRoutes = require('./home-routes.js');
 
-router.use('/', homeRoutes);
-
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/', homeRoutes);
+
 
 module.exports = router;
